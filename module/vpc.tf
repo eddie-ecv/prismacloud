df7357105c972846bee1e3cd0c344bfa5bd46a29
eddie.cheung <eddie.cheung@ecloudvalley.com>
2024-03-01 16:59:00 +0800

    First Commit

---
+++ module/vpc.tf
@@ -0,0 +1,50 @@
+# Create VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# Create NAT Gateway
+resource "aws_nat_gateway" "my_nat_gateway" {
+  allocation_id = aws_eip.my_eip.id
+  subnet_id     = aws_subnet.my_public_subnet[0].id
+}
+
+# Create Public Subnets
+resource "aws_subnet" "my_public_subnet" {
+  count             = 2
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.${count.index}.0/24"
+  availability_zone = "us-west-2a"
+}
+
+# Create Private Subnets
+resource "aws_subnet" "my_private_subnet" {
+  count             = 2
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.${count.index + 10}.0/24"
+  availability_zone = "us-west-2a"
+}
+
+# Create Route Table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# Associate Public Subnets with Route Table
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = 2
+  subnet_id      = aws_subnet.my_public_subnet[count.index].id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+# Associate Private Subnets with Route Table
+resource "aws_route_table_association" "private_subnet_association" {
+  count          = 2
+  subnet_id      = aws_subnet.my_private_subnet[count.index].id
+  route_table_id = aws_route_table.my_route_table.id
+}
